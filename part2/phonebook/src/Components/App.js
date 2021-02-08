a5f445e1b671480cee92164babb8f409d96ce5ce
Miko Mynttinen <mikokm@gmail.com>
2021-02-08 15:42:04 +0100

    2.11

--- part2/phonebook/src/Components/App.js
+++ part2/phonebook/src/Components/App.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+         .then(response => setPersons(response.data))
+  }, [])
 
   const [filterText, setFilterText] = useState('')
   const [newName, setNewName] = useState('')
