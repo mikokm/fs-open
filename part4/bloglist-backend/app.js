92a724df01298c0440785dcd0eaf648d484859df
Miko Mynttinen <mikokm@gmail.com>
2021-02-22 19:16:52 +0100

    4.15

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
